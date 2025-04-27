0c296855c98f2081950f53f1fd92f2166d281eed
mykulas <terka@markt.cz>
2025-04-27 13:26:09 +0200

    feat: added adding variables to variable display

--- src/variablesdisplay.cpp
+++ src/variablesdisplay.cpp
@@ -1,4 +1,7 @@
 #include "variablesdisplay.h"
+#include <qdialog.h>
+#include <qlineedit.h>
+#include <qdialogbuttonbox.h>
 #include "ui_variablesdisplay.h"
 
 VariablesDisplay::VariablesDisplay(QWidget *parent)
@@ -6,13 +9,20 @@ VariablesDisplay::VariablesDisplay(QWidget *parent)
     , ui(new Ui::VariablesDisplay)
 {
     ui->setupUi(this);
+
+    typeVar[INPUTV] = ui->lblInputVar;
+    typeVar[OUTPUTV] = ui->lblOutputVar;
+    typeVar[INTERNALV] = ui->lblInternalVar;
+
     connect(ui->btnHide, &QPushButton::clicked,this, &VariablesDisplay::hideOrShow);
-    connect(ui->btnAddOutputVar, &QPushButton::clicked, this, [this](){insertVariable(ui->lblOutputVar);});
-    connect(ui->btnAddInputVar, &QPushButton::clicked, this, [this](){insertVariable(ui->lblInputVar);});
-    connect(ui->btnAddInternalVar, &QPushButton::clicked, this, [this](){insertVariable(ui->lblInternalVar);});
+    connect(ui->btnAddOutputVar, &QPushButton::clicked, this, [this](){getVariableInfoInsert(OUTPUTV);});
+    connect(ui->btnAddInputVar, &QPushButton::clicked, this, [this](){getVariableInfoInsert(INPUTV);});
+    connect(ui->btnAddInternalVar, &QPushButton::clicked, this, [this](){getVariableInfoInsert(INTERNALV);});
+
     ui->btnRemoveInputVar->setDisabled(true);
     ui->btnRemoveOutputVar->setDisabled(true);
     ui->btnRemoveInternalVar->setDisabled(true);
+
     ui->btnEditInputVar->setDisabled(true);
     ui->btnEditOutputVar->setDisabled(true);
     ui->btnEditInternalVar->setDisabled(true);
@@ -21,6 +31,14 @@ VariablesDisplay::VariablesDisplay(QWidget *parent)
 VariablesDisplay::~VariablesDisplay()
 {
     delete ui;
+    /*
+    for(int i = 0; i < NUMV; i++){
+        for(FSMVariable v : allVars[i]){
+            delete v.name;
+            delete v.value;
+        }
+    }
+    */
 }
 
 void VariablesDisplay::hideOrShow(){
@@ -35,17 +53,48 @@ void VariablesDisplay::hideOrShow(){
     ui->scrollArea->setVisible(shown);
 }
 
-void VariablesDisplay::insertVariable(QLabel *type){
-    QString name = "name";
-    QString value = "value";
+void VariablesDisplay::insertVariable(enum variableType type, QString name, QString value){
     QFormLayout *layout = qobject_cast<QFormLayout*>(ui->scrollAreaWidgetContents->layout());
 
     QLabel *newLblName = new QLabel(name, ui->scrollAreaWidgetContents);
     QLabel *newLblValue = new QLabel(value, ui->scrollAreaWidgetContents);
 
     int row;
-    layout->getWidgetPosition(type, &row, nullptr);
+    layout->getWidgetPosition(typeVar[type], &row, nullptr);
     layout->insertRow(row + 1, newLblName, newLblValue);
-
+    FSMVariable h{newLblName,newLblValue};
+    allVars[type].append(h);
 }
 
+
+void VariablesDisplay::getVariableInfoInsert(enum variableType type){
+    // make dialog for getting neccassary info
+    QDialog dialog(this);
+    dialog.setStyleSheet("QWidget{background-color: white}");
+    dialog.setWindowTitle("Resize work-area");
+
+    QFormLayout form(&dialog);
+
+    // two spinboxes (width, height)
+    QLineEdit *nameInput = new QLineEdit(&dialog);
+    form.addRow("Name:", nameInput);
+
+    // OK + Cancel buttons
+    QDialogButtonBox buttonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel, Qt::Horizontal, &dialog);
+    connect(&buttonBox, &QDialogButtonBox::accepted, &dialog, &QDialog::accept);
+    connect(&buttonBox, &QDialogButtonBox::rejected, &dialog, &QDialog::reject);
+
+    if(type == OUTPUTV){
+        form.addRow(&buttonBox);
+        if (dialog.exec() == QDialog::Accepted && nameInput->text() != "") {
+            insertVariable(type,nameInput->text(),"");
+        }
+    }else{
+        QLineEdit *valueInput = new QLineEdit(&dialog);
+        form.addRow("Value:", valueInput);
+        form.addRow(&buttonBox);
+        if (dialog.exec() == QDialog::Accepted && nameInput->text() != "" && valueInput->text() != "") {
+            insertVariable(type,nameInput->text(),valueInput->text());
+        }
+    }
+}
--- src/variablesdisplay.h
+++ src/variablesdisplay.h
@@ -14,27 +14,40 @@ class VariablesDisplay : public QWidget
     Q_OBJECT
 
 public:
+
+    enum variableType{
+        INPUTV,
+        OUTPUTV,
+        INTERNALV,
+        NUMV
+    };
+
     explicit VariablesDisplay(QWidget *parent = nullptr);
     ~VariablesDisplay();
     void hideOrShow();
     /**
      * @brief inserts a variable
-     * @param under which label the new variable belongs
+     * @param type under which label the new variable belongs
      */
-    void insertVariable(QLabel *type);
+    void insertVariable(enum variableType type, QString name, QString value);
+
+    /**
+     * @brief gets all necessary info for variables
+     * @param type under which label the new variable belongs
+     */
+    void getVariableInfoInsert(enum variableType type);
 
 private:
     struct FSMVariable {
-        QPushButton *name;
+        QLabel *name;
         QLabel *value;
     };
 
     Ui::VariablesDisplay *ui;
     bool shown = true;
 
-    QVector<FSMVariable> inputVar;
-    QVector<FSMVariable> outputVar;
-    QVector<FSMVariable> internalVar;
+    QVector<FSMVariable> allVars[3];
+    QLabel *typeVar[3];
 };
 
 #endif // VARIABLESDISPLAY_H
