3f5a115ee362167f29254ee96620140d6a43006b
xcerv <xcervia00@stud.fit.vutbr.cz>
2025-05-08 12:45:54 +0200

    feat: left/right also cycle inputs

--- src/view/input_event_edit/input_event_line_edit.cpp
+++ src/view/input_event_edit/input_event_line_edit.cpp
@@ -18,15 +18,28 @@ InputEventLineEdit::InputEventLineEdit(QWidget *parent) : QLineEdit(parent)
 
 void InputEventLineEdit::keyPressEvent(QKeyEvent *event)
 {
-    if (event->key() == Qt::Key_Up) {
-        emit upArrowPressed();
-        QLineEdit::keyPressEvent(event);
-    } else if (event->key() == Qt::Key_Down) {
-        emit downArrowPressed();
-        QLineEdit::keyPressEvent(event);
+    if(!event) return;
+
+    switch(event->key())
+    {
+        //              ^
+        //       Or     |
+        // --->         |
+        case Qt::Key_Up:
+        case Qt::Key_Right:
+            emit upArrowPressed();
+            break;
+
+        //              |
+        //       Or     |
+        // <---         V
+        case Qt::Key_Down:
+        case Qt::Key_Left:
+            emit downArrowPressed();
+            break;
+        default:
+            break;
     }
 
-    if (!(event->key() == Qt::Key_Up || event->key() == Qt::Key_Down) || !event->isAccepted()) {
-        QLineEdit::keyPressEvent(event);
-    }
+    QLineEdit::keyPressEvent(event);
 }
